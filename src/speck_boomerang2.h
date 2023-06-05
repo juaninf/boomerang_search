0866b15502bb473fc4e7221f2e07f1f3cb5e3af1
Juan del Carmen Grados Vasquez <juan.grados@tii.ae>
2023-06-05 09:20:45 +0400

    Adding print states solution

--- src/speck_boomerang2.h
+++ src/speck_boomerang2.h
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <random>
 #include<fstream>
+#include "ortools_extend_sat.h"
 
 using std::cout;
 using std::endl;
@@ -77,4 +78,25 @@ std::string vectorToString(const std::vector<int>& vec) {
 
     return result;
 }
+
+std::string binaryToHex(const std::string& binaryString, int bit_size) {
+    std::bitset<256> bits(binaryString);  // Assuming 256-bit binary string, adjust the size as needed
+    std::stringstream hexStream;
+    hexStream << std::hex << std::setw((bit_size + 3) / 4) << std::setfill('0') << bits.to_ulong();
+    return hexStream.str();
+}
+
+void print_states(std::vector< std::array<BoolVec, 2> > allState, int branch_size, operations_research::sat::CpSolverResponse response) {
+
+    for (int k = 0; k < allState.size(); k++) {
+        std::vector<int> tmp;
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < branch_size; j++)
+                tmp.push_back(SolutionIntegerValue(response, allState[k][i][j]));
+        cout<<binaryToHex(vectorToString(tmp), 2*branch_size)<<endl;
+    }
+}
+
+
+
 #endif //SEARCH_SPECK_BOOMERANG2_CPP_H
